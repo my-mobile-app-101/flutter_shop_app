deb6d70a746094f55e8849ddbebe2c1dbbf9fab6
zhouxiaojun <zhouxiaojun@gg.com>
2021-11-23 15:45:20 +0800

    修改一些配置

--- flutter_girlshop/lib/config/http_conf.dart
+++ flutter_girlshop/lib/config/http_conf.dart
@@ -1,4 +1,4 @@
-const base_url = 'http://10.10.20.16:8081/';
+const base_url = 'http://10.10.20.16:8080/';
 const servicePath = {
   'homePageContext':base_url + 'getHomePageContent',//首页数据
   'getHotGoods':base_url + 'getHotGoods',//火爆专区
